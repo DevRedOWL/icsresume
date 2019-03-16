581d0590ba7e7e1873b3d767758faa91d65d7084
Daniil <vda.perm@mail.ru>
2019-03-16 18:36:52 +0500

    Add files via upload

---
+++ docs/js/main.js
@@ -0,0 +1,66 @@
+var ThisRest; // Ну на всякий случай сохраним, че нам
+var ServerUrl = "https://icsresume.herokuapp.com/invoices";
+
+// При загрузке документа
+$(document).ready(function() {
+  $('#sended').hide();
+  $('#unsended').hide();
+  GotIt();
+});
+
+// Метод получения
+function GotIt() {
+  $('#ElementsList tr').slice(1).remove(); // Очищаем таблицу
+  $.get(ServerUrl, onAjaxSuccess);
+}
+
+// Метод, выполняющийся при успехе GET запроса
+function onAjaxSuccess(data) {
+  ThisRest = data; // Запоминаем текущее состояние
+  console.log(ThisRest); // Выводим в консоль для отладки
+  // Выводим все элементы в таблицу
+  $.each(ThisRest, function(index, val){
+        newline = "<tr><th>"+val.date_created+"</th><th>"+val.number+"</th><th>"+val.date_due+"</th><th>"+val.comment+"</th><th class ='ds'><i class='fas fa-trash-alt'></i></th></tr>"
+        $('#ElementsList').append(newline);
+    });
+}
+
+// Метод добавления
+function PostThat() {
+  // Упрощаем использование значений полей
+  var num = document.getElementById('Number').value;
+  var sup = document.getElementById('Supply').value;
+  var inv = document.getElementById('Invoice').value;
+  var com = document.getElementById('Comment').value;
+  datte = new Date(); var dat = datte.toLocaleDateString();
+  // Если все поля, кроме комментария заполнены
+  if (num != 0 && sup != 0 && inv != 0)
+  {
+    // Создаем пост запрос на основании аргументов
+    $.post(ServerUrl, { comment: com, date_created: dat, date_due: inv, date_supply: sup, number: num  });
+    // Показываем уведомление о добавлении
+    $('#sended').fadeIn('fast'); setTimeout(function() {  $('#sended').fadeOut('slow'); }, 3000); $('#unsended').hide(); // .show
+    // Добавляем строку в таблицу
+    newline = "<tr><th>" + dat + "</th><th>" + num + "</th><th>" + inv + "</th><th>" + com + "</th><th class ='ds'><i class='fas fa-trash-alt'></i></th></tr>"
+    $('#ElementsList').append(newline);
+  }
+  // Если поля не заполнены
+  else
+  {
+    $('#unsended').fadeIn('fast'); setTimeout(function() {  $('#unsended').fadeOut('slow'); }, 3000);
+    $('#sended').hide(); //alert("Все поля должны быть заполнены");
+  }
+}
+
+// Метод удаления
+function GoToHell(goddamn){
+  $.get(ServerUrl, function(data) {
+  var xml = $(data);
+
+  xml.find(goddamn).text(goddamn);
+
+  $.post({ comment: 1, date_created: 1, date_due: 1, date_supply: 1, number: 1  }, xml, function(resp) {
+    alert(resp);
+  }, "xml");
+});
+}
